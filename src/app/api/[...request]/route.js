77576a3715b48f1c96d056f4e35e9ee849e9c1e1
BholuSingh <60701111+BHOLU-SINGH@users.noreply.github.com>
2023-09-10 15:35:13 +0530

    initial commit

--- src/app/api/[...request]/route.js
+++ src/app/api/[...request]/route.js
@@ -6,6 +6,50 @@ export async function GET(request, content) {
   let path = content.params.request;
   let newPath;
 
+  const data = () => {
+    return (
+      newPath === "movie/now_playing" ||
+      newPath === "movie/popular" ||
+      newPath === "movie/top_rated" ||
+      newPath === "movie/upcoming" ||
+      newPath === "tv/airing_today" ||
+      newPath === "tv/on_the_air" ||
+      newPath === "tv/popular" ||
+      newPath === "tv/top_rated"
+    );
+  };
+
+  if (path[path.length - 2] === "page") {
+    if (path[0] === "movie" || path[0] === "tv") {
+      let pageNo = path[path.length - 1];
+      let rmPage = path;
+      rmPage.pop();
+      rmPage.pop();
+
+      newPath = "" + rmPage;
+      newPath = newPath.replaceAll(",", "/");
+      newPath = newPath.replaceAll("-", "_");
+
+      if (path.length == 1) {
+        console.log("length is : "+path.length)
+        newPath = path[0];
+        newPath === "movie"
+          ? (newPath = "movie/now_playing")
+          : (newPath = "tv/airing_today");
+      }
+
+      let data = await fetch(
+        api + newPath + "?api_key=" + API_KEY + "&page=" + pageNo
+      );
+      let result = await data.json();
+      return NextResponse.json({ result, success: true });
+    } else {
+      return NextResponse.json({
+        result: "Invalid API, Please try again!",
+        success: false,
+      });
+    }
+  }
   if (path.length == 1) {
     if (path[0] === "movie" || path[0] === "tv") {
       path[0] === "movie"
@@ -15,10 +59,46 @@ export async function GET(request, content) {
       let result = await data.json();
       return NextResponse.json({ result, success: true });
     }
+  } else if (path.length == 2) {
+    newPath = path[0] + "/" + path[1].replaceAll("-", "_");
+    if (data()) {
+      let data = await fetch(api + newPath + "?api_key=" + API_KEY);
+      let result = await data.json();
+      return NextResponse.json({ result, success: true });
+    } else {
+      const Id = content.params.request[1];
+      const data = await fetch(
+        api + path[0] + "/" + Id + "?api_key=" + API_KEY
+      );
+      const result = await data.json();
+      return NextResponse.json({ result, success: true });
+    }
+  } else if (path.length == 3) {
+    newPath = path[0] + "/" + path[1].replaceAll("-", "_");
+    const page = path[path.length - 1];
+    if (data()) {
+      const data = await fetch(
+        api + path[0] + "/" + page + "?api_key=" + API_KEY
+      );
+      const result = await data.json();
+      return NextResponse.json({ result, success: true });
+    }
   } else {
     return NextResponse.json({
       result: "Invalid API, Please try again!",
       success: false,
     });
+  }
+
+  if (path[0] + "/" + path[1] + "/" === "s/" + path[1] + "/") {
+    const searchItem =
+      "discover/" +
+      path[1] +
+      "?include_adult=false&include_video=false&language=en-US&page=1&year=";
+    const data = await fetch(
+      api + searchItem + path[2] + "&api_key=" + API_KEY
+    );
+    const result = await data.json();
+    return NextResponse.json({ result, success: true });
   }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,8 @@ import Button from "./Components/Button";
 import Footer from "./Components/Footer";
 import Link from "next/link";
 
-
 const getMoviesList = async () => {
-  let data = await fetch("http://localhost:3000/api/movie/");
+  let data = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=04bb17c0897894ac573e81dad9bd0d64");
   let response = await data.json();
   return response.result;
 };
@@ -18,10 +17,11 @@ export default async function Page() {
     <main className="container">
       <Header />
       <div className="content center">
+        <div className="searchBox">lorem500</div>
         <div className="cardBox">
           {res.results.map((movie) => (
             <>
-              <div className="card">
+              <div className="card" key={movie.id}>
                 <Image
                   src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
                   alt={movie.title}
@@ -46,6 +46,37 @@ export default async function Page() {
             </>
           ))}
         </div>
+      </div>
+      <div className="pagination">
+        <ul className="center">
+          <li className="active">
+            <Link href="1">1</Link>
+          </li>
+          <li>
+            <Link href="2">2</Link>
+          </li>
+          <li>
+            <Link href="3">3</Link>
+          </li>
+          <li>
+            <Link href="4">4</Link>
+          </li>
+          <li>
+            <Link href="5">5</Link>
+          </li>
+          <li>
+            <Link href="6">6</Link>
+          </li>
+          <li>
+            <Link href="#">...</Link>
+          </li>
+          <li>
+            <Link href="74">74</Link>
+          </li>
+          <li>
+            <Link href="2">next</Link>
+          </li>
+        </ul>
       </div>
       <Footer />
     </main>
