435fbb3bbb8c3ece9d65735d86c1b4bfe9ffa318
BholuSingh <60701111+BHOLU-SINGH@users.noreply.github.com>
2023-09-10 14:05:26 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,8 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Link from "next/link";
 
-const getMoviesList = async () => {
-  let data = await fetch("http://localhost:3000/api/movie/");
-  let response = await data.json();
-  return response.result;
-};
 
 export default async function Page() {
-  const res = await getMoviesList();
 
   return (
     <main className="container">
