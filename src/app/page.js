f111a7eacf70346d22fa8ac6882ec0f9c2a142af
BholuSingh <60701111+BHOLU-SINGH@users.noreply.github.com>
2023-09-10 14:59:30 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,36 +5,46 @@ import Footer from "./Components/Footer";
 import Link from "next/link";
 
 
-export default function Page() {
+const getMoviesList = async () => {
+  let data = await fetch("http://localhost:3000/api/movie/");
+  let response = await data.json();
+  return response.result;
+};
+
+export default async function Page() {
+  const res = await getMoviesList();
 
   return (
     <main className="container">
       <Header />
       <div className="content center">
         <div className="cardBox">
-
+          {res.results.map((movie) => (
+            <>
               <div className="card">
                 <Image
-                  src="flixify.png"
-                  alt="image loading error"
+                  src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                  alt={movie.title}
                   width={200}
                   height={50}
                   priority
                 />
-                <span className="title">The Godfather</span>
+                <span className="title">{movie.title}</span>
                 <div>
-                  <span>01/01/2001</span>
-                  <span className="popularity">9.1⭐</span>
+                  <span>{movie.release_date}</span>
+                  <span className="popularity">{movie.vote_average}⭐</span>
                 </div>
                 <span className="overview">
-                  <small className="ow-title">Title: The Godfather</small>
+                  <small className="ow-title">Title: {movie.title}</small>
                   <small className="ow-rel-date">
-                    Release Date: 01/01/2001
+                    Release Date: {movie.release_date}
                   </small>
-                  <small className="overw">OverView: This is a movie</small>
-		  <Button id="#" page="movie" />
+                  <small className="overw">OverView: {movie.overview}</small>
+                  <Button id={movie.id} page="movie" />
                 </span>
               </div>
+            </>
+          ))}
         </div>
       </div>
       <Footer />
