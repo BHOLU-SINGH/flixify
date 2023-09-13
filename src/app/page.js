f6308294d9699dad437a936804e81f6fdd0b0c02
BholuSingh <60701111+BHOLU-SINGH@users.noreply.github.com>
2023-09-13 17:10:42 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,15 @@ import Button from "./Components/Button";
 import Footer from "./Components/Footer";
 import Link from "next/link";
 
-const getMoviesList = async () => {
-  let data = await fetch("http://localhost:3000/api/movie/");
-  let response = await data.json();
-  return response.result;
-};
+async function getServerSideProps() {
+  const response = await fetch("http://localhost:3000/api/movie");
+  const data = response.json();
+  return data;
+}
 
-export default async function Page() {
-  const res = await getMoviesList();
+export default function Page() {
+  let data = await getServerSideProps();
+  data = data.result.results;
 
   return (
     <main className="container">
@@ -19,31 +20,29 @@ export default async function Page() {
       <div className="content center">
         <div className="searchBox">lorem500</div>
         <div className="cardBox">
-          {res.results.map((movie) => (
-            <>
-              <div className="card" key={movie.id}>
-                <Image
-                  src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-                  alt={movie.title}
-                  width={200}
-                  height={50}
-                  priority
-                />
-                <span className="title">{movie.title}</span>
-                <div>
-                  <span>{movie.release_date}</span>
-                  <span className="popularity">{movie.vote_average}⭐</span>
-                </div>
-                <span className="overview">
-                  <small className="ow-title">Title: {movie.title}</small>
-                  <small className="ow-rel-date">
-                    Release Date: {movie.release_date}
-                  </small>
-                  <small className="overw">OverView: {movie.overview}</small>
-                  <Button id={movie.id} page="movie" />
-                </span>
+          {data.map((item) => (
+            <div className="card" key={item.id}>
+              <Image
+                src={"https://image.tmdb.org/t/p/w500" + item.poster_path}
+                alt={item.title}
+                width={200}
+                height={50}
+                priority
+              />
+              <span className="title">{item.title}</span>
+              <div>
+                <span>{item.release_date}</span>
+                <span className="popularity">{item.vote_average}⭐</span>
               </div>
-            </>
+              <span className="overview">
+                <small className="ow-title">Title: {item.title}</small>
+                <small className="ow-rel-date">
+                  Release Date: {item.release_date}
+                </small>
+                <small className="overw">OverView: {item.overview}</small>
+                <Button id={item.id} page="movie" />
+              </span>
+            </div>
           ))}
         </div>
       </div>
