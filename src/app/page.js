3ec820457424f0fb519aebb80b1931dbc2dd14d5
BholuSingh <60701111+BHOLU-SINGH@users.noreply.github.com>
2023-09-17 16:47:40 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,77 @@ import Footer from "./Components/Footer";
 import Link from "next/link";
 const apiKey = process.env.API_KEY;
 
+const getServerSideProps = async (props) => {
+  let response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key="+props);	
+  const data = await response.json();
+  return data;
+};
+
 
 export default async function page() {
+const res = await getServerSideProps(apiKey);
   return (
     <main className="container">
       <Header />
       <div className="content center">
-	<p>APi key is : {apiKey}</p>
+        {/* <div className="searchBox">lorem500</div> */}
+        <div className="cardBox">
+          {res.results.map((item) => (
+            <div className="card" key={item.id}>
+              <Image
+                src={"https://image.tmdb.org/t/p/w500" + item.poster_path}
+                alt={item.title}
+                width={200}
+                height={50}
+                priority
+              />
+              <span className="title">{item.title}</span>
+              <div>
+                <span>{item.release_date}</span>
+                <span className="popularity">{item.vote_average}⭐</span>
+              </div>
+              <span className="overview">
+                <small className="ow-title">Title: {item.title}</small>
+                <small className="ow-rel-date">
+                  Release Date: {item.release_date}
+                </small>
+                <small className="overw">OverView: {item.overview}</small>
+                <Button id={item.id} page="movie" />
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="pagination">
+        <ul className="center">
+          <li className="active">
+            <Link href="1">1</Link>
+          </li>
+          <li>
+            <Link href="2">2</Link>
+          </li>
+          <li>
+            <Link href="3">3</Link>
+          </li>
+          <li>
+            <Link href="4">4</Link>
+          </li>
+          <li>
+            <Link href="5">5</Link>
+          </li>
+          <li>
+            <Link href="6">6</Link>
+          </li>
+          <li>
+            <Link href="#">...</Link>
+          </li>
+          <li>
+            <Link href="74">74</Link>
+          </li>
+          <li>
+            <Link href="2">next</Link>
+          </li>
+        </ul>
       </div>
       <Footer />
     </main>
