b067c8382521780c4bfc2c95bbd4c140869fd139
BholuSingh <60701111+BHOLU-SINGH@users.noreply.github.com>
2023-09-20 17:46:33 +0530

    initial commit

---
+++ src/app/movie/now-playing/page.js
@@ -0,0 +1,23 @@
+import Header from "@/app/Components/Header";
+import MovieCard from "@/app/Components/MovieCard";
+import Footer from "@/app/Components/Footer";
+
+const { API_URL, API_KEY } = process.env;
+
+async function getServerSideProps() {
+    const API = API_URL + "movie/now_playing?api_key=" + API_KEY;
+    const response = await fetch(API);
+    const data = await response.json();
+    return data;
+}
+
+export default async function page() {
+    const data = await getServerSideProps();
+    return (
+        <div className="container">
+            <Header />
+            <MovieCard data={data} page="movie/now-playing" />
+            <Footer />
+        </div>
+    );
+}
