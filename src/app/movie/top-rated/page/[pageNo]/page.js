4c9d9535fb98166f993363f886f2ea2b5767579c
BholuSingh <60701111+BHOLU-SINGH@users.noreply.github.com>
2023-09-20 18:13:31 +0530

    initial commit

---
+++ src/app/movie/top-rated/page/[pageNo]/page.js
@@ -0,0 +1,24 @@
+import MovieCard from "@/app/Components/MovieCard";
+
+const { API_URL, API_KEY } = process.env;
+
+async function getServerSideProps(pageNo) {
+    const API = API_URL + "movie/top_rated?api_key=" + API_KEY + "&page=" + pageNo;
+    const response = await fetch(API);
+    const data = await response.json();
+    return data;
+}
+
+
+const page = async (props) => {
+    const pageNo = props.params.pageNo;
+    const data = await getServerSideProps(pageNo);
+
+    return (
+        <div className="container">
+            <MovieCard data={data} page={"movie/top-rated/page/" + pageNo} />
+        </div>
+    );
+}
+
+export default page;
